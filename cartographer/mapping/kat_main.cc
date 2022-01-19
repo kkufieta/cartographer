11a0ebb7d0cca98dbda4e1739227164b397b0f7a
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-01-19 17:44:26 -0500

    It still compiles and runs!

--- cartographer/mapping/kat_main.cc
+++ cartographer/mapping/kat_main.cc
@@ -28,6 +28,7 @@
 #include "cartographer/metrics/register.h"
 #include "cartographer/common/lua_parameter_dictionary.h"
 #include "cartographer/common/configuration_file_resolver.h"
+#include "cartographer/mapping/internal/testing/test_helpers.h"
 #include "gflags/gflags.h"
 
 DEFINE_string(configuration_directory, "",
@@ -38,6 +39,15 @@ DEFINE_string(configuration_basename, "",
               "Basename, i.e. not containing any directory prefix, of the "
               "configuration file.");
 
+using SensorId = cartographer::mapping::TrajectoryBuilderInterface::SensorId;
+
+const SensorId kRangeSensorId{SensorId::SensorType::RANGE, "range"};
+constexpr double kDuration = 4.;         // Seconds.
+constexpr double kTimeStep = 0.1;        // Seconds.
+constexpr double kTravelDistance = 1.2;  // Meters.
+
+std::vector<::cartographer::transform::Rigid3d> local_slam_result_poses_;
+
 std::unique_ptr<::cartographer::common::LuaParameterDictionary> LoadOptions(
     const std::string& configuration_directory,
     const std::string& configuration_basename) {
@@ -47,6 +57,8 @@ std::unique_ptr<::cartographer::common::LuaParameterDictionary> LoadOptions(
   const std::string lua_code =
       file_resolver->GetFileContentOrDie(configuration_basename);
 
+
+  // Use this instead of NonReferenceCounted if you want to check lua files:
   // return absl::make_unique<::cartographer::common::LuaParameterDictionary>(
   //     lua_code, std::move(file_resolver));
 
@@ -55,20 +67,47 @@ std::unique_ptr<::cartographer::common::LuaParameterDictionary> LoadOptions(
   return luaParameterDictionary;
 }
 
+cartographer::mapping::MapBuilderInterface::LocalSlamResultCallback GetLocalSlamResultCallback() {
+    return [=](const int trajectory_id, const ::cartographer::common::Time time,
+               const ::cartographer::transform::Rigid3d local_pose,
+               ::cartographer::sensor::RangeData range_data_in_local,
+               const std::unique_ptr<
+                   const cartographer::mapping::TrajectoryBuilderInterface::
+                       InsertionResult>) {
+      local_slam_result_poses_.push_back(local_pose);
+    };
+  }
 
 void Run(const std::string& configuration_directory,
          const std::string& configuration_basename) {
   std::cout << "Start Run\n";
+  auto options = LoadOptions(configuration_directory, configuration_basename);
 
-  // auto options = LoadOptions(configuration_directory, configuration_basename);
+  auto map_builder_parameters = options->GetDictionary("map_builder");
+  auto trajectory_builder_parameters = options->GetDictionary("trajectory_builder");
 
-  // auto map_builder = mapping::CreateMapBuilder(
-  //     map_builder_server_options.map_builder_options());
-  // std::unique_ptr<MapBuilderServerInterface> map_builder_server =
-  //     CreateMapBuilderServer(map_builder_server_options,
-  //                            std::move(map_builder));
-  // map_builder_server->Start();
-  // map_builder_server->WaitForShutdown();
+  auto map_builder_options_ =
+        cartographer::mapping::CreateMapBuilderOptions(map_builder_parameters.get());
+  auto trajectory_builder_options_ =
+        cartographer::mapping::CreateTrajectoryBuilderOptions(trajectory_builder_parameters.get());
+
+  auto map_builder_ = cartographer::mapping::CreateMapBuilder(map_builder_options_);
+
+  int trajectory_id = map_builder_->AddTrajectoryBuilder(
+      {kRangeSensorId}, trajectory_builder_options_,
+      GetLocalSlamResultCallback());
+
+  cartographer::mapping::TrajectoryBuilderInterface* trajectory_builder =
+      map_builder_->GetTrajectoryBuilder(trajectory_id);
+
+  const auto measurements = cartographer::mapping::testing::GenerateFakeRangeMeasurements(
+      kTravelDistance, kDuration, kTimeStep);
+
+  for (const auto& measurement : measurements) {
+    trajectory_builder->AddSensorData(kRangeSensorId.id, measurement);
+  }
+  map_builder_->FinishTrajectory(trajectory_id);
+  map_builder_->pose_graph()->RunFinalOptimization();
   std::cout << "End Run\n";
 }
 
