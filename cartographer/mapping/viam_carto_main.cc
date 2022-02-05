3893e7a3cd2678302060e5cf53706f625966f8d5
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-02-04 20:18:20 -0500

    Add code to paint data

--- cartographer/mapping/viam_carto_main.cc
+++ cartographer/mapping/viam_carto_main.cc
@@ -3,6 +3,10 @@
 #include "cartographer/metrics/register.h"
 #include <string>
 
+#include "cartographer/io/submap_painter.h"
+#include "cartographer/io/image.h"
+#include "cartographer/io/file_writer.h"
+
 namespace cartographer {
 namespace mapping {
 
@@ -12,6 +16,48 @@ const SensorId kRangeSensorId{SensorId::SensorType::RANGE, "range"};
 const SensorId kIMUSensorId{SensorId::SensorType::IMU, "imu"};
 
 
+void PaintMap(std::unique_ptr<cartographer::mapping::MapBuilderInterface> & map_builder_, int i) {
+  const double kPixelSize = 0.05;
+  const auto submap_poses = map_builder_->pose_graph()->GetAllSubmapPoses();
+  std::map<cartographer::mapping::SubmapId, ::cartographer::io::SubmapSlice> submap_slices;
+
+  std::cout << "size of submap_poses: " << submap_poses.size() << std::endl;
+  for (const auto& submap_id_pose : submap_poses) {
+    cartographer::mapping::proto::SubmapQuery::Response response_proto;
+    const std::string error = map_builder_->SubmapToProto(submap_id_pose.id, &response_proto);
+
+    auto submap_textures = absl::make_unique<::cartographer::io::SubmapTextures>();
+    submap_textures->version = response_proto.submap_version();
+    for (const auto& texture_proto : response_proto.textures()) {
+      const std::string compressed_cells(texture_proto.cells().begin(),
+                                         texture_proto.cells().end());
+      submap_textures->textures.emplace_back(::cartographer::io::SubmapTexture{
+          ::cartographer::io::UnpackTextureData(compressed_cells, texture_proto.width(),
+                                                texture_proto.height()),
+          texture_proto.width(), texture_proto.height(), texture_proto.resolution(),
+          cartographer::transform::ToRigid3(texture_proto.slice_pose())});
+    }
+
+    // Prepares SubmapSlice
+    ::cartographer::io::SubmapSlice& submap_slice = submap_slices[submap_id_pose.id];
+    const auto fetched_texture = submap_textures->textures.begin();
+    submap_slice.width = fetched_texture->width;
+    submap_slice.height = fetched_texture->height;
+    std::cout << "width, height: " << submap_slice.width << ", " << submap_slice.height << std::endl;
+    submap_slice.slice_pose = fetched_texture->slice_pose;
+    submap_slice.resolution = fetched_texture->resolution;
+    submap_slice.cairo_data.clear();
+    submap_slice.surface = ::cartographer::io::DrawTexture(
+        fetched_texture->pixels.intensity, fetched_texture->pixels.alpha, fetched_texture->width,
+        fetched_texture->height, &submap_slice.cairo_data);
+  }
+  // Generates occupancy grid as CreateOccupancyGridMsg()
+  cartographer::io::PaintSubmapSlicesResult painted_slices =
+      PaintSubmapSlices(submap_slices, kPixelSize);
+  auto image = cartographer::io::Image(std::move(painted_slices.surface));
+  auto file = cartographer::io::StreamFileWriter("pictures/map_" + std::to_string(i) + ".png");
+  image.WritePng(&file);
+}
 
 void Run(std::string mode, std::string data_directory, const std::string& configuration_directory,
          const std::string& configuration_basename) {
@@ -45,17 +91,20 @@ void Run(std::string mode, std::string data_directory, const std::string& config
 
   //std::cout << "Beginning to add data....\n";
   //while (true) {
+  int j = 1;
   for (size_t i = 0; i < file_list.size(); i++ ) { // file_list.size()
     //std::cout << "Data [" << i << "]....\n";
     auto measurement = mapBuilderViam.GenerateSavedRangeMeasurements(0.0, float(i), 1.0, initial_file, i, data_directory);// kTravelDistance, kDuration, kTimeStep); // change!
 
     if (measurement.ranges.size() > 0) {
         trajectory_builder->AddSensorData(kRangeSensorId.id, measurement);
+        PaintMap(mapBuilderViam.map_builder_, j++);
     }
   }
 
   mapBuilderViam.map_builder_->FinishTrajectory(trajectory_id);
   mapBuilderViam.map_builder_->pose_graph()->RunFinalOptimization();
+  PaintMap(map_builder_, 0);
 
   if (mode == "Global2D") {
       const auto trajectory_nodes = mapBuilderViam.map_builder_->pose_graph()->GetTrajectoryNodes();
@@ -88,7 +137,7 @@ int main(int argc, char** argv) {
     data_directory = argv[1];
   }
   else {
-    data_directory = "/home/jeremyhyde-viam/data";
+    data_directory = "/home/kkufieta/rplidar/data";
     std::cout << "No data directory specified, using default: " << data_directory << std::endl;
   }
 
